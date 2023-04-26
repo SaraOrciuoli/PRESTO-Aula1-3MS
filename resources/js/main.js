a6cb1614ab9bd1c29ebbef5ed65ae98dd649e832
SabrinaCherubini <sabrina.cherubini@hotmail.com>
2023-04-26 20:12:17 +0200

    filtro per categoria

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,7 +17,7 @@ fetch('resources/js/Annunci.json')
     function setCategory() {
       let uniqueCategories = [];
       data.forEach(element => {
-        
+
         if (!uniqueCategories.includes(element.category)) {
           uniqueCategories.push(element.category);
 
@@ -28,6 +28,71 @@ fetch('resources/js/Annunci.json')
 
     }
     setCategory();
+    let wrapper = document.querySelector(".wrapper");
+    
+    function CreateCards(array) {
+      wrapper.innerHTML="";
+      array.forEach(element => {
+      let div = document.createElement('div');
+        div.classList.add('card', 'col-3', 'pt-2', 'm-3');
+        div.innerHTML =
+          `<img src="./resources/media/Nike.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"> ${element.name}</h5>
+      <p class="card-text">${element.price}€</p>
+      <a href="#" class="btn-card">Add to cart</a>
+    </div>`;
+
+        wrapper.appendChild(div);
+      })
+
+    }
+    CreateCards(data);
+
+    let btnAll=document.querySelector("#all");
+    let btnMen=document.querySelector('#men');
+    let btnWomen=document.querySelector('#women');
+    let btnKids=document.querySelector('#kids');
+    let btnShoes=document.querySelector('#shoes');
+    let btnAccessories=document.querySelector('#accessories');
+  
+
+  function filterByCategory(category) {
+  let filtered=data.filter(element=> element.category==category );
+
+  console.log(filtered);
+  CreateCards(filtered);
+}
+ btnMen.addEventListener('click',()=>{
+  filterByCategory("Men");
+ })
+
+ btnAll.addEventListener('click',()=> {
+  CreateCards(data);
+ })
+
+ btnWomen.addEventListener('click',()=>{
+  filterByCategory("Women");
+ })
+
+ btnKids.addEventListener('click',()=>{
+  filterByCategory("Kids");
+ })
+
+ btnShoes.addEventListener('click',()=>{
+  filterByCategory("Shoes");
+ })
+
+ btnAccessories.addEventListener('click',()=>{
+  filterByCategory("Accessories");
+ })
+ 
   })
 
-  
+
+
+
+
+
+
+
