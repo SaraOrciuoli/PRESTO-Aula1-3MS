11e98e4e53d5049cfd36c458a398d60c116c989e
Michele Di Corato <micheled.46@hotmail.it>
2023-04-27 09:40:08 +0200

    Pulizia formattazione codice

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,34 +9,29 @@ window.addEventListener("scroll", () => {
   }
 });
 
-fetch('resources/js/Annunci.json')
-  .then(response => response.json())
-  .then(data => {
+fetch("resources/js/Annunci.json")
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
 
     function setCategory() {
       let uniqueCategories = [];
-      data.forEach(element => {
-
+      data.forEach((element) => {
         if (!uniqueCategories.includes(element.category)) {
           uniqueCategories.push(element.category);
-
         }
-
       });
       console.log(uniqueCategories);
-
     }
     setCategory();
     let wrapper = document.querySelector(".wrapper");
-    
+
     function CreateCards(array) {
-      wrapper.innerHTML="";
-      array.forEach(element => {
-      let div = document.createElement('div');
-        div.classList.add('card', 'col-3', 'pt-2', 'm-3');
-        div.innerHTML =
-          `<img src="./resources/media/Nike.jpg" class="card-img-top" alt="...">
+      wrapper.innerHTML = "";
+      array.forEach((element) => {
+        let div = document.createElement("div");
+        div.classList.add("card", "col-3", "pt-2", "m-3");
+        div.innerHTML = `<img src="./resources/media/Nike.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title"> ${element.name}</h5>
       <p class="card-text">${element.price}€</p>
@@ -44,55 +39,44 @@ fetch('resources/js/Annunci.json')
     </div>`;
 
         wrapper.appendChild(div);
-      })
-
+      });
     }
     CreateCards(data);
 
-    let btnAll=document.querySelector("#all");
-    let btnMen=document.querySelector('#men');
-    let btnWomen=document.querySelector('#women');
-    let btnKids=document.querySelector('#kids');
-    let btnShoes=document.querySelector('#shoes');
-    let btnAccessories=document.querySelector('#accessories');
-  
+    let btnAll = document.querySelector("#all");
+    let btnMen = document.querySelector("#men");
+    let btnWomen = document.querySelector("#women");
+    let btnKids = document.querySelector("#kids");
+    let btnShoes = document.querySelector("#shoes");
+    let btnAccessories = document.querySelector("#accessories");
 
-  function filterByCategory(category) {
-  let filtered=data.filter(element=> element.category==category );
+    function filterByCategory(category) {
+      let filtered = data.filter((element) => element.category == category);
 
-  console.log(filtered);
-  CreateCards(filtered);
-}
- btnMen.addEventListener('click',()=>{
-  filterByCategory("Men");
- })
+      console.log(filtered);
+      CreateCards(filtered);
+    }
+    btnMen.addEventListener("click", () => {
+      filterByCategory("Men");
+    });
 
- btnAll.addEventListener('click',()=> {
-  CreateCards(data);
- })
+    btnAll.addEventListener("click", () => {
+      CreateCards(data);
+    });
 
- btnWomen.addEventListener('click',()=>{
-  filterByCategory("Women");
- })
+    btnWomen.addEventListener("click", () => {
+      filterByCategory("Women");
+    });
 
- btnKids.addEventListener('click',()=>{
-  filterByCategory("Kids");
- })
+    btnKids.addEventListener("click", () => {
+      filterByCategory("Kids");
+    });
 
- btnShoes.addEventListener('click',()=>{
-  filterByCategory("Shoes");
- })
+    btnShoes.addEventListener("click", () => {
+      filterByCategory("Shoes");
+    });
 
- btnAccessories.addEventListener('click',()=>{
-  filterByCategory("Accessories");
- })
- 
-  })
-
-
-
-
-
-
-
-
+    btnAccessories.addEventListener("click", () => {
+      filterByCategory("Accessories");
+    });
+  });
